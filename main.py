dfe9909190751711448a911f56a16a4246e40b93
Manu-RMT <mandu543@gmail.com>
2024-01-14 21:17:02 +0100

    ajout code dans requirements

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ path_source_html = "D:/TD_DATALAKE/DATALAKE/0_SOURCE_WEB/"
 path_landing_zone = "D:/TD_DATALAKE/DATALAKE/1_LANDING_ZONE/"
 path_curated_zone = "D:/TD_DATALAKE/DATALAKE/2_CURATED_ZONE/"
 
-# ## Landing ZONE
-# # deplacement dans landing_zone
-# deplacement_fichier(path_source_html, path_landing_zone)
+## Landing ZONE
+# deplacement dans landing_zone
+deplacement_fichier(path_source_html, path_landing_zone)
 
-# # création de la métadonnées techniques
-# creation_metadata_technique(path_source_html,path_landing_zone)
+# création de la métadonnées techniques
+creation_metadata_technique(path_source_html,path_landing_zone)
 
-# ## Curated ZONE
-# # deplacement dans curate_zone
-# deplacement_fichier(path_source_html, path_curated_zone,"curated_zone")
+## Curated ZONE
+# deplacement dans curate_zone
+deplacement_fichier(path_source_html, path_curated_zone,"curated_zone")
 
-#Création des métadonnées descriptif
+# Création des métadonnées descriptif
 creation_metadata_descriptif(path_landing_zone,path_curated_zone)
 
