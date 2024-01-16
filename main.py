e858ada08d477bed6c4ce16d434726d6c3158f3c
Manu-RMT <mandu543@gmail.com>
2024-01-16 10:50:24 +0100

    changement chemin répertoire

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import os
 from tools_functions import *
 
-# path_source_html = "D:/COURS LYON 2/COURS M2/TD Gouvernence des données massives/TD_DATALAKE/DATALAKE/0_SOURCE_WEB/" 
-# path_landing_zone = "D:/COURS LYON 2/COURS M2/TD Gouvernence des données massives/TD_DATALAKE/DATALAKE/1_LANDING_ZONE/"
-# path_curated_zone = "D:/COURS LYON 2/COURS M2/TD Gouvernence des données massives/TD_DATALAKE/DATALAKE/2_CURATED_ZONE/"
-
-path_source_html = "D:/TD_DATALAKE/DATALAKE/0_SOURCE_WEB/"
-path_landing_zone = "D:/TD_DATALAKE/DATALAKE/1_LANDING_ZONE/"
-path_curated_zone = "D:/TD_DATALAKE/DATALAKE/2_CURATED_ZONE/"
+path_source_html = "C:/TD_DATALAKE/DATALAKE/0_SOURCE_WEB/"
+path_landing_zone = "C:/TD_DATALAKE/DATALAKE/1_LANDING_ZONE/"
+path_curated_zone = "C:/TD_DATALAKE/DATALAKE/2_CURATED_ZONE/"
 
 ## Landing ZONE
 # deplacement dans landing_zone
@@ -22,4 +18,3 @@ deplacement_fichier(path_source_html, path_curated_zone,"curated_zone")
 
 # Création des métadonnées descriptif
 creation_metadata_descriptif(path_landing_zone,path_curated_zone)
-
